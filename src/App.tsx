eb011fdfa294ae5252fe7720d9e37599aa47b2fd
habiba2m <habibamohamadeen@gmail.com>
2024-07-28 14:47:36 +0300

    enhance: add calculations of items number

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -150,10 +150,24 @@ function Item({
   );
 }
 
-function Stats() {
+function Stats({ items }: { items: itemType[] }) {
+  if (!items.length) {
+    return <footer className="stats">🧳 No items on your list yet! 🧳</footer>;
+  }
+
+  const numItems = items.length;
+  const numItemsPacked = items.filter((item) => item.packed).length;
+  const percentage = numItems === 0 ? 0 : (numItemsPacked / numItems) * 100;
+
   return (
     <footer className="stats">
-      🧳 You have X items to pack, and Y items packed.
+      {percentage === 100 ? (
+        <div>🎉 You have packed all your items! 🎉</div>
+      ) : (
+        `🧳 You have ${numItems} items on your list, and you have already packed 
+      ${numItemsPacked} ${numItemsPacked === 1 ? "item" : "items"} 
+      (${Math.round(percentage)}%).`
+      )}
     </footer>
   );
 }
